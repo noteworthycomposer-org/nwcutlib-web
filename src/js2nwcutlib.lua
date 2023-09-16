fcadae6807e9f539eda057c43cd49fe4dafbf4e2
Noteworthy Software, Inc <online@noteworthysoftware.com>
2023-09-16 13:49:36 -0400

    Give up on webpack, switching to esbuild

---
+++ src/js2nwcutlib.lua
@@ -0,0 +1,73 @@
+--print('js2nwc loading')
+return function(luaPreloads)
+	--luaPreloads = {
+	--	['outstream'] = require('./outstream.lua'),
+	--	['synthesized-nwc.lua'] = require('nwcutlib/synthesized-nwc.lua'),
+	--	['synthesize.lua'] = require('nwcutlib/synthesize.lua'),
+	--	['simulation.lua'] = require('nwcutlib/simulation.lua'),
+	--	['nwcut.lua'] = require('nwcutlib/nwcut.lua')
+	--}
+	--print('js2nwc init function',type(luaPreloads),#luaPreloads,luaPreloads)
+	--for k,v in pairs(luaPreloads) do print(k,v) end
+
+	local fengary_loadfile = loadfile
+	local fengary_dofile = dofile
+	local fengary_print = print
+	local smart_loadfile = function(filename,mode,env)
+		local m = luaPreloads[filename]
+		if not m then return fengary_loadfile(filename,mode,env) end
+		-- avoid creating a nil default environment
+		return env and load(m,filename,mode,env) or load(m,filename,mode)
+	end
+
+	loadfile = smart_loadfile
+	dofile = function(f)
+		if luaPreloads[f] then
+			local m = smart_loadfile(f)
+			return m and m() or nil
+		end
+
+		return fengary_dofile(f)
+	end
+
+	local NWOutStream = smart_loadfile('outstream')()
+
+	local function getSafeString(s)
+		return (type(s) == 'string') and s or ''
+	end
+
+	local function runUserTool(mod,intxt,...)
+		arg = {...}
+		intxt = tostring(intxt) or ''
+		local io_stdout = NWOutStream.new()
+		local io_stderr = NWOutStream.new()
+		local function stdout_writer(...) io_stdout:write(...) end
+		io = {write=stdout_writer,stdout=io_stdout,stderr=io_stderr}
+		print = function(...) io_stdout:writeln(...) end
+
+		local doExecuteTool = function()
+			dofile('synthesize.lua')(intxt)
+			nwcut.run(arg[1])
+		end
+		local good,failmsg = pcall(doExecuteTool)
+		if not good then
+			nwcut.status = 1
+			io_stderr:writeln('A run-time error was detected')
+			io_stderr:writeln(failmsg)
+		end
+
+		print = fengary_print
+		return {
+			status=nwcut.status,
+			output=js.tostring(io_stdout),
+			error=js.tostring(io_stderr)
+		}
+	end
+
+	local nwcRunUserTool = function(mod,tool,nwctxt,...)
+		local r = runUserTool(mod,nwctxt,tool,...)
+		return js.createproxy(r,'object')
+	end
+
+	return js.createproxy(nwcRunUserTool,'function')
+end
--- src/main.js
+++ src/main.js
@@ -1,8 +1,45 @@
-import {nwcRunUserTool} from './nwcutlib.lua-js';
+import {L,lua,lauxlib,interop,to_luastring} from 'fengari-web';
+
+import lua_js2nwcutlib from './js2nwcutlib.lua';
+import lua_outstream from './outstream.lua';
+import lua_synthesized_nwc from 'nwcutlib/synthesized-nwc.lua';
+import lua_synthesize from 'nwcutlib/synthesize.lua';
+import lua_simulation from 'nwcutlib/simulation.lua';
+import lua_nwcut from 'nwcutlib/nwcut.lua';
+
+const {LUA_OK,lua_createtable,lua_gettop,lua_pcall,lua_pop,lua_pushcfunction,lua_pushvalue,lua_pushliteral,lua_setfield,lua_tostring,lua_type} = lua;
+const {luaL_loadbuffer} = lauxlib;
+const {tojs} = interop;
+
 
 function init() {
+	// the approach here is to bundle all of the nwcutlib Lua files, then set them up as a preload
+	// passed to the js2nwcutlib.lua init file. It then handles the rest from the Lua side.
+	function lua_preload(name,source) {
+		lua_pushliteral(L, source);
+    	lua_setfield(L, -2, to_luastring(name));
+	}
+	
+	let ok = luaL_loadbuffer(L, to_luastring(lua_js2nwcutlib), null, to_luastring('js2nwcutlib.lua'));
+	if (ok !== LUA_OK) {
+		if (ok === LUA_ERRSYNTAX) throw new SyntaxError(lua_tojsstring(L, -1));
+		else throw tojs(L, -1);
+	}
+	if (lua_pcall(L, 0, 1, 0) != LUA_OK) throw tojs(L, -1);
+	// init function is now at the top of the stack
+	lua_createtable(L);
+	lua_preload('outstream',lua_outstream);
+	lua_preload('synthesized-nwc.lua',lua_synthesized_nwc);
+	lua_preload('synthesize.lua',lua_synthesize);
+	lua_preload('simulation.lua',lua_simulation);
+	lua_preload('nwcut.lua',lua_nwcut);
+
+	if (lua_pcall(L, 1, 1, 0) != LUA_OK) console.log(tojs(L, -1),'Error');
+	let nwcRunUserTool = tojs(L, -1);
+	lua_pop(L, 1);
+
 	let jslib = {
-		version: VERSION,
+		version: PKG_VERSION,
 		run: nwcRunUserTool
 	};
 	
