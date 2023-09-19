ec07f9fce1e70df3afc8813c0d9be0ddcc9e808f
Noteworthy Software, Inc <online@noteworthysoftware.com>
2023-09-19 00:26:21 -0400

    Simplify esbuild process (remove separate luamin loader)

--- esbuild.js
+++ esbuild.js
@@ -1,7 +1,21 @@
+const fs = require('fs');
 const { build } = require('esbuild');
 const { replace } = require('esbuild-plugin-replace');
-const { luamin } = require('./loader/luamin');
+const luamin = require('luamin');
 const pkg_json = require('./package.json');
+
+let luamin_loader = (options = {}) => {
+	return {
+		name: 'luamin',
+		setup(build) {
+			build.onLoad({ filter: options.include }, async (args) => {
+				const source = await fs.promises.readFile(args.path, "utf8");
+				const contents = luamin.minify(source);
+				return { contents, loader: 'default' };				
+			});
+		}
+	}
+}
 
 build({
 	entryPoints: ['./src/main.js'],
@@ -29,6 +43,6 @@ build({
 				'PKG_VERSION': JSON.stringify(pkg_json.version)
 			}
 		}),
-		luamin({include: /\.lua$/})
+		luamin_loader({include: /\.lua$/})
 	],
 })
