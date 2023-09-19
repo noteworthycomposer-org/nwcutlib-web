e5e8533c19b616acda19e5bbb1da9dca72da740f
Noteworthy Software, Inc. <online@noteworthysoftware.com>
2023-09-19 01:05:57 -0400

    Update, attempt replace of 'esbuild-plugin-replace'

--- esbuild.js
+++ esbuild.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const { build } = require('esbuild');
-const { replace } = require('esbuild-plugin-replace');
 const luamin = require('luamin');
 const pkg_json = require('./package.json');
+const { replace } = require('esbuild-plugin-replace');
 
 let luamin_loader = (options = {}) => {
 	return {
@@ -11,6 +11,22 @@ let luamin_loader = (options = {}) => {
 			build.onLoad({ filter: options.include }, async (args) => {
 				const source = await fs.promises.readFile(args.path, "utf8");
 				const contents = luamin.minify(source);
+				return { contents, loader: 'default' };				
+			});
+		}
+	}
+}
+
+// hope to use something like this eventually to replace 'esbuild-plugin-replace'
+let newreplace = (options = {}) => {
+	return {
+		name: 'newreplace',
+		setup(build) {
+			build.onLoad({ filter: options.include }, async (args) => {
+				let contents = await fs.promises.readFile(args.path, "utf8");
+				for (const k in Object.keys(options.values)) {
+					contents = contents.replaceAll(k,options.values[k]);
+				}
 				return { contents, loader: 'default' };				
 			});
 		}
