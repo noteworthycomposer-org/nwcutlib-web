4b532f7b5a3f088910f9732f1e19643adb2d8fe1
Noteworthy Software, Inc. <online@noteworthysoftware.com>
2023-10-06 07:20:34 -0400

    Trying out maquettejs for prompting

--- src/nwcut_prompt.js
+++ src/nwcut_prompt.js
@@ -1,30 +1,33 @@
+import {h,createProjector} from 'maquette';
 
+const projector = createProjector();
+
+// before maquette: 247kb (gzip 82009 bytes)
+// after maquette: 257.1kb (gzip 85699 bytes)
 function nwcut_prompt(msg,  datatype, v3, v4,callbackFunc) {
 	let listvals = (datatype == '|') ? v3 : false;
 	let defaultval = listvals ? v4 : v3;
-	let c_dlg = document.createElement('dialog');
-	let c_form = c_dlg.appendChild(document.createElement('form'));
-	let c_label = document.createElement('label');
-	let c_input = document.createElement('input');
-	c_label.setAttribute('for',c_input.getAttribute('id'));
-	c_form.appendChild(c_label);
-	c_form.appendChild(c_input);
-	let c_ok = c_form.appendChild(document.createElement('button'));
-	c_dlg.className = 'nwcut';
-	c_label.textContent = msg;
-	c_input.type = 'text';
-	c_input.value = defaultval;
-	c_ok.type = 'submit';
-    c_ok.textContent = 'Submit';
-	document.body.appendChild(c_dlg);
-	c_form.addEventListener('submit', (e) => {
-		e.preventDefault();
-		let r = c_input.value;
-		c_dlg.remove();
-		c_dlg = null;
-		callbackFunc(r);
-	});
-	c_dlg.showModal();
+	function doValChange(e) {
+		defaultval = e.target.value;
+	}
+	function doSubmit(e) {
+		document.querySelector('dialog.nwcut').close();
+		projector.detach(renderprompt);
+		callbackFunc(defaultval);
+	}
+	function renderprompt() {
+		return h('dialog.nwcut',[
+			h('form',{onsubmit: doSubmit}, [
+				h('label', [
+					msg,
+					h('input', {type: 'text', value: defaultval, oninput: doValChange})
+				]),
+				h('button',{type: 'submit'}, ['Submit'])
+			])
+		]);
+	}
+	projector.append(document.body, renderprompt);
+	document.querySelector('dialog.nwcut').showModal();
 }
 
 function nwcut_prompt_promise(msg,  datatype, v3, v4) {
